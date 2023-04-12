142e39b10df3da5c8100820b45cd968f79ba9437
gerryik <33073924+gerryik@users.noreply.github.com>
2023-04-12 14:40:00 +0100

    Update appshbredlibrary.groovy

--- vars/appshbredlibrary.groovy
+++ vars/appshbredlibrary.groovy
@@ -1,12 +1,18 @@
-def projApp(String 'appURL') {
+def call(String 'repoURL') {
 pipeline{
   agent any
+  tools {
+     maven 'maven'
+  }
   stages{
-    stage('project9'){
+    stage('Parallel stage'){
       parallel{     
         stage('Gerald Agbonye'){
           steps{
             sh '/var/lib/jenkins/script/project9grp5q1scrpit.sh'
+            sh 'mvn --version'
+            git branch: 'main',
+              url: "${repoURL}"
           }
         }
         stage('Pretei Lemo'){
