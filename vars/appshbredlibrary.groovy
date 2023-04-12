5bcaee4c5718fc1929ea0259a3a47419a06b73fd
gerryik <33073924+gerryik@users.noreply.github.com>
2023-04-12 13:15:35 +0100

    Create appshbredlibrary.groovy

---
+++ vars/appshbredlibrary.groovy
@@ -0,0 +1,37 @@
+def projApp(String 'appURL') {
+pipeline{
+  agent any
+  stages{
+    stage('project9'){
+      parallel{     
+        stage('Gerald Agbonye'){
+          steps{
+            sh '/var/lib/jenkins/script/project9grp5q1scrpit.sh'
+          }
+        }
+        stage('Pretei Lemo'){
+          steps{
+            sh '/var/lib/jenkins/script/lemorscript.sh'
+          }
+        }
+   stage('Pere'){
+       steps{
+            echo "TODO"
+        }
+    }
+    stage('Odile Domingo'){
+       steps{
+           sh '/var/lib/jenkins/script/odilescript.sh'
+         }
+     }
+        stage("Henry Forson") {
+           steps {
+               echo "TODO"
+            }
+          }
+        }
+     }
+   }
+}
+
+}
